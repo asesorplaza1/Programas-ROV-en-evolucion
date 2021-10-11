0d459f80d562309f48e0f81a6b67c9608a575708
asesorplaza1 <66756078+asesorplaza1@users.noreply.github.com>
2021-10-11 23:34:28 +0200

    Delete 02_Esclavo_19_09_2021 directory

--- 02_Esclavo_19_09_2021/Temp_DS18B20.ino
+++
@@ -1,9 +0,0 @@
-
-void loop_DS18B20() {  // Bloque de trabajo del sensor de temperatura DS18B20
-
-  sensorDS18B20.requestTemperatures();
-  Serial.print (F("\n Temperatura sensor DS18B20 = "));
-  Serial.print (sensorDS18B20.getTempCByIndex(0));
-  Serial.print (F(" ºC"));
-  txdata.sensorDS18B20 = sensorDS18B20.getTempCByIndex(0);
-}
